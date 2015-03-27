18c8085481251d75725efa7faf0bb422460087d0
Elais Player <elais.player@gmail.com>
2015-03-26 21:14:43 -0500

    init commit

--- src/main/java/edu/uab/cis/probability/ngram/NgramLanguageModel.java
+++ src/main/java/edu/uab/cis/probability/ngram/NgramLanguageModel.java
@@ -1,10 +1,15 @@
 package edu.uab.cis.probability.ngram;
 
 import java.util.List;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 /**
  * A probabilistic n-gram language model.
- * 
+ *
  * @param <T>
  *          The type of items in the sequences over which the language model
  *          estimates probabilities.
@@ -17,7 +22,7 @@ public class NgramLanguageModel<T> {
      * have its joint probability P(w<sub>1</sub>,...,w<sub>n</sub>) estimated
      * as #(w<sub>1</sub>,...,w<sub>n</sub>) / N, where N indicates the total
      * number of all 1-grams observed during training.
-     * 
+     *
      * Note that we have defined only the joint probability of an n-gram here.
      * Deriving the conditional probability from the definition above is left as
      * an exercise.
@@ -32,7 +37,7 @@ public class NgramLanguageModel<T> {
      * #(w<sub>1</sub>,...,w<sub>n-1</sub>)), where # indicates the number of
      * times an n-gram was observed during training and V indicates the number
      * of <em>unique</em> 1-grams observed during training.
-     * 
+     *
      * Note that Laplace smoothing defines only the conditional probability of
      * an n-gram, not the joint probability.
      */
@@ -57,7 +62,7 @@ public class NgramLanguageModel<T> {
 
   /**
    * Creates an n-gram language model.
-   * 
+   *
    * @param n
    *          The number of items in an n-gram.
    * @param representation
@@ -65,40 +70,55 @@ public class NgramLanguageModel<T> {
    * @param smoothing
    *          The type of smoothing to apply when estimating probabilities.
    */
+  private int ngram_size;
+  private Representation representation;
+  private Smoothing smoothing;
   public NgramLanguageModel(int n, Representation representation, Smoothing smoothing) {
     // TODO
+    this.ngram_size = n;
+    this.representation = representation;
+    this.smoothing = smoothing;
   }
 
   /**
    * Trains the language model with the n-grams from a sequence of items.
-   * 
+   *
    * This typically involves collecting counts of n-grams that occurred in the
    * sequence.
-   * 
+   *
    * @param sequence
    *          The sequence on which the model should be trained.
    */
   public void train(List<T> sequence) {
     // TODO
+    String[] s = sequence.toArray(new String[sequence.size()]);
+    List<String> ngram_list = createNgrams(this.ngram_size, s);
+    Map<String, Double> ngrams = new HashMap<String, Double>();
+    for(String ngram : ngram_list)
+      ngrams.putIfAbsent(ngram, 0.0);
+    for
+
+
+
   }
 
   /**
    * Return the estimated n-gram probability of the sequence:
-   * 
+   *
    * P(w<sub>0</sub>,...,w<sub>k</sub>) = ∏<sub>i=0,...,k</sub>
    * P(w<sub>i</sub>|w<sub>i-n+1</sub>, w<sub>i-n+2</sub>, ..., w<sub>i-1</sub>)
-   * 
+   *
    * For example, a 3-gram language model would calculate the probability of the
    * sequence [A,B,B,C,A] as:
-   * 
+   *
    * P(A,B,B,C,A) = P(A)*P(B|A)*P(B|A,B)*P(C|B,B)*P(A|B,C)
-   * 
+   *
    * The exact calculation of the conditional probabilities in this expression
    * depends on the smoothing method. See {@link Smoothing}.
-   * 
+   *
    * The result is in the range [0,1] with {@link Representation#PROBABILITY}
    * and in the range (-∞,0] with {@link Representation#LOG_PROBABILITY}.
-   * 
+   *
    * @param sequence
    *          The sequence of items whose probability is to be estimated.
    * @return The estimated probability of the sequence.
@@ -106,5 +126,20 @@ public class NgramLanguageModel<T> {
   public double probability(List<T> sequence) {
     // TODO
     return 0.0;
+  }
+  
+
+  public List<String> createNgrams(int n, String[] s){
+    List<String> ngrams = new ArrayList<String>();
+    for (int i = 0; i < s.length - n + 1; i++)
+      ngrams.add(concat(s, i, i+n));
+    return ngrams;
+  }
+  
+  public static String concat(String[] words, int start, int end){
+    StringBuilder sb = new StringBuilder();
+    for(int i = start; i < end; i++)
+      sb.append((i > start ? "" : "") + words[i]);
+    return sb.toString();
   }
 }
